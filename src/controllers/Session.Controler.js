8892c19e3109dbe1f073158c40c906f5577f42a5
KAIQUE <cg3027481@labA204m23.IFSP>
2024-05-14 08:44:15 -0300

    Configurando a session

--- src/controllers/Session.Controler.js
+++
@@ -1,8 +0,0 @@
-import User from "../models/user";
-import * as yup from 'yup'
-
-class SessionControler{
-  async store(req, res){
-    
-  }
-}
---
+++ src/router.js
@@ -0,0 +1,7 @@
+import { Router } from 'express'
+import multer from 'multer'
+import uploadConfig from './config/upload'
+import SessionController from './controllers/SessionControler'
+
+const routes = new Router()
+
