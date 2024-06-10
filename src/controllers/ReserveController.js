977106ff623cd6d032f7ea967b4d5cc1c7778d04
KAIQUE <cg3027481@labA204m23.IFSP>
2024-06-10 08:45:54 -0300

    Configurando o Dashboard Controller

--- src/controllers/ReserveController.js
+++ src/controllers/ReserveController.js
@@ -7,6 +7,8 @@ class ReserveController {
     const { user_id } = req.headers;
 
     const reserves =  await Reserve.find({ user: user_id }).populate('house');
+
+    return res.json(reserves);
   }
 
   async store(req, res) {
